77d7c5e1a7969fe46f8880c3ae848399ce93f4cb
imaneboho2159 <imaboh21@gmail.com>
2025-04-10 11:55:46 +0100

    adding the projects components

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,1 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
+  imports: [CommonModule] ,
+})
+export class ProjectsComponent {
+  projects = [
+    {
+      title: 'Application_de_Gestion_de_Projets_de_Construction',
+      description: 'Cette application web permettra à l\'équipe de construction de créer et de gérer\n' +
+        'des projets de\n' +
+        'construction, d\'assigner des tâches aux membres de l\'équipe, de gérer les\n' +
+        'ressources nécessaires et\n' +
+        'de suivre les détails des projets et des tâches. Elle offrira une interface conviviale\n' +
+        'pour visualiser,\n',
+      link: 'https://github.com/imaneboho2159/Application_de_Gestion_de_Projets_de_Construction'
+    },
+
+
+    {
+      title: 'Todo + Pomodoro App',
+      description: 'Task manager with local storage and Pomodoro timer.',
+      link: 'https://github.com/imaneboho2159/To-Do-List-Promodoro-App',
+    }
+  ];
+}
 
